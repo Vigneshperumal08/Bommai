51634fbaf96eacdf272b256ef59a692cc11c3e77
Vigneshperumal E <146161790+Vigneshperumal08@users.noreply.github.com>
2025-01-12 00:23:23 +0530

    Update index.js

--- client/src/store/shop/products-slice/index.js
+++ client/src/store/shop/products-slice/index.js
@@ -18,7 +18,7 @@ export const fetchAllFilteredProducts = createAsyncThunk(
     });
 
     const result = await axios.get(
-      `https://bommai-1.onrender.com/api/shop/products/get?${query}`
+      `https://bommaikaran-labs.onrender.com/api/shop/products/get?${query}`
     );
 
     console.log(result);
@@ -31,7 +31,7 @@ export const fetchProductDetails = createAsyncThunk(
   "/products/fetchProductDetails",
   async (id) => {
     const result = await axios.get(
-      `https://bommai-1.onrender.com/api/shop/products/get/${id}`
+      `https://bommaikaran-labs.onrender.com/api/shop/products/get/${id}`
     );
 
     return result?.data;
